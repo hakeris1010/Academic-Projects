234ce1afdb01087e0beacc722a149cdf8b9c95ca
hakeris1010 <hakeris1010@gmail.com>
2016-03-06 20:39:48 +0200

    Split code into different files.

---
+++ Others/Binary Search Trees/treestructs.h
@@ -0,0 +1,12 @@
+#ifndef TREESTRUCTS_H_INCLUDED
+#define TREESTRUCTS_H_INCLUDED
+
+struct TreeNode
+{
+    int inf;
+    int height;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+#endif // TREESTRUCTS_H_INCLUDED
---
+++ Others/Binary Search Trees/visualisation.h
@@ -0,0 +1,8 @@
+#ifndef VISUALISATION_H_INCLUDED
+#define VISUALISATION_H_INCLUDED
+
+#include "treestructs.h"
+
+void showBinaryTree(TreeNode* tree, int mode=0);
+
+#endif // VISUALISATION_H_INCLUDED
